16213e4a126de4ee6037d7fb2ce2c258902cc86b
FajarNurIlhamm <fajarnurilham17@gmail.com>
2024-01-04 17:05:29 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
   <h2>aloo</h2>
   <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure id omnis natus sint reiciendis adipisci tenetur, asperiores, aspernatur sed alias voluptates accusamus consectetur deleniti dignissimos enim quia maiores corrupti ullam.</p>
+
+  <ul>
+    <li>satu</li>
+    <li>dua</li>
+  </ul>
 </body>
 </html>
